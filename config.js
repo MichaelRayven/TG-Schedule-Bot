0a91f65d135ec9cf722a1244933e7bac3f5dbedd
Michael Rayven <themikerayven@gmail.com>
2024-12-03 01:03:47 +0700

    Updated text

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export const SEARCH_ROUTE = "/studies/schedule/schedule_classes?query="
 export const SCHEDULE_ROUTE = "/studies/schedule/schedule_classes/schedule?group="
 // ?group=<NAME>&week=<NUMBER>
 
-export const welcomeMessage = `Добро пожаловать в ###, здесь вы можете быстро узнать расписание дня.
+export const welcomeMessage = `Добро пожаловать в ScheduleBot , здесь вы можете быстро узнать расписание дня.
 Для дополнительной информации используйте команду /help`
 
-export const helpMessage = `Help message`
+export const helpMessage = `Список команд: \n/set_group - выбор вашей группы \n/add_note - добавить заметку \n/check_off - отметить заметку как выполненную \n/delete_note - удалить заметку`
